0d6e541bb82040c69924abb2858549e93d0be980
Sanito Yapoyco <yapoyco.sanito@gmail.com>
2024-03-01 21:08:52 +0800

    init 3.0

--- server.js
+++ server.js
@@ -7,4 +7,12 @@ const PORT = 1234;
 app.use(cors());
 app.use(express.json());
 
+
+app.use('/test', async (req, res) => {
+    res.status(200).json({
+        message: 'okay ra bai',
+        status: 200
+    })
+})
+
 app.listen(PORT, () => console.log(`Running na sirr at ${PORT}!`));
