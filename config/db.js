bc36e67d33f0ef5f18079b2bfc3ce8a9e985af09
Sanito Yapoyco <yapoyco.sanito@gmail.com>
2024-03-01 20:28:25 +0800

    init

---
+++ config/db.js
@@ -0,0 +1,22 @@
+const { Pool } = require('pg')
+
+
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'hackathon_db',
+    password: 'root',
+    port: 5432,
+})
+
+
+module.exports = {
+    query: (statement, params) => pool.query(statement, params)
+}
+
+// PORT=3000
+// PGUSER=postgres
+// PGHOST=localhost
+// PGDATABASE=lending_system
+// PGPASSWORD=root
+// PGPORT=5432
